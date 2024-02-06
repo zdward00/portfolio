ce7e210ea4954242db5cbba0c3561056538a0a65
zdward00 <59321359+zdward00@users.noreply.github.com>
2024-02-06 18:27:20 +0000

    added composer.json, funcs.php, index.php, features.json, etc

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php echo file_get_contents("html/header.html"); ?>
-<?php echo file_get_contents("html/body.html"); ?>
-<?php echo file_get_contents("html/footer.html"); ?>
-<p>Copyright © CM Cawley <?php echo date("Y"); ?></p> 
